dd2bfe7352254495e6ece9518dc210cf4a46d1b7
wangand1145 <3844896275@qq.com>
2026-08-22 13:58:21 +0800

    Create TetraUltimateMaterial.java

---
+++ src/main/java/com/yourname/tetra_ultimate_material/TetraUltimateMaterial.java
@@ -0,0 +1,16 @@
+package com.yourname.tetra_ultimate_material;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("tetra_ultimate_material")
+public class TetraUltimateMaterial {
+
+    public TetraUltimateMaterial() {
+        // 物品注册
+        ModItems.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        // 方块注册
+        ModBlocks.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        ModBlocks.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+}
