017b16f9db1088370d620ab43b20be65e502a0eb
wangand1145 <3844896275@qq.com>
2026-08-24 22:38:31 +0800

    Update ModRecipes.java

--- src/main/java/com/wangand1145/tetra_ultimate_material/registry/ModRecipes.java
+++ src/main/java/com/wangand1145/tetra_ultimate_material/registry/ModRecipes.java
@@ -1,20 +1,22 @@
 package com.wangand1145.tetra_ultimate_material.registry;
 
-import com.wangand1145.tetra_ultimate_material.recipe.TorchCraftRecipe;
-import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.item.crafting.RecipeSerializer;
 
 public class ModRecipes {
-    public static final DeferredRegister<RecipeSerializer<?>> SERIALIZERS =
+
+    private static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS =
             DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, "tetra_ultimate_material");
 
-    public static final RegistryObject<RecipeSerializer<TorchCraftRecipe>> TORCH_CRAFT =
-            SERIALIZERS.register("torch_craft", () -> TorchCraftRecipe.TorchCraftRecipeSerializer.INSTANCE);
+    // 示例占位，当前未注册任何配方序列化器
+    // public static final RegistryObject<RecipeSerializer<?>> EXAMPLE = RECIPE_SERIALIZERS.register("example", () -> null);
 
-    public static void register(IEventBus bus) {
-        SERIALIZERS.register(bus);
+    private ModRecipes() {}
+
+    public static void register(IEventBus modEventBus) {
+        RECIPE_SERIALIZERS.register(modEventBus);
     }
 }
