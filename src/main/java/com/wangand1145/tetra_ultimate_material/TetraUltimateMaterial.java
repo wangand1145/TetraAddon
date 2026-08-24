5b116509c44a433b27e8f9061a0d1dbfb9bf0d5c
wangand1145 <3844896275@qq.com>
2026-08-24 12:15:36 +0800

    Update TetraUltimateMaterial.java

--- src/main/java/com/wangand1145/tetra_ultimate_material/TetraUltimateMaterial.java
+++ src/main/java/com/wangand1145/tetra_ultimate_material/TetraUltimateMaterial.java
@@ -1,8 +1,9 @@
 package com.wangand1145.tetra_ultimate_material;
 
+import com.wangand1145.tetra_ultimate_material.registry.ModRecipes;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.eventbus.api.IEventBus;
 
 @Mod("tetra_ultimate_material")
 public class TetraUltimateMaterial {
@@ -10,7 +11,8 @@ public class TetraUltimateMaterial {
     public TetraUltimateMaterial() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        ModItems.ITEMS.register(bus);    // ✅ 注册所有物品（含基泥、苔煤团、苔煤锭、两个方块物品）
-        ModBlocks.BLOCKS.register(bus);  // ✅ 注册所有方块（基泥块、致密基泥块）
+        ModItems.ITEMS.register(bus);      // 物品
+        ModBlocks.BLOCKS.register(bus);    // 方块
+        ModRecipes.register(bus);          // 配方序列化器 ← 新加的这一行
     }
 }
