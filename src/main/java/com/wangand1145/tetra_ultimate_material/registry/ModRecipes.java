0a797ce315c25a50060a1741dec535bf233e1e3c
wangand1145 <3844896275@qq.com>
2026-08-24 12:14:16 +0800

    Refactor recipe serializers to use SERIALIZERS

--- src/main/java/com/wangand1145/tetra_ultimate_material/registry/ModRecipes.java
+++ src/main/java/com/wangand1145/tetra_ultimate_material/registry/ModRecipes.java
@@ -8,14 +8,13 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModRecipes {
-    public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS = 
-        DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, "tetra_ultimate_material");
-    
-    public static final RegistryObject<RecipeSerializer<TorchCraftRecipe>> TORCH_CRAFT = 
-        RECIPE_SERIALIZERS.register("torch_craft", 
-            () -> TorchCraftRecipe.TorchCraftRecipeSerializer.INSTANCE);
-    
+    public static final DeferredRegister<RecipeSerializer<?>> SERIALIZERS =
+            DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, "tetra_ultimate_material");
+
+    public static final RegistryObject<RecipeSerializer<TorchCraftRecipe>> TORCH_CRAFT =
+            SERIALIZERS.register("torch_craft", () -> TorchCraftRecipe.TorchCraftRecipeSerializer.INSTANCE);
+
     public static void register(IEventBus bus) {
-        RECIPE_SERIALIZERS.register(bus);
+        SERIALIZERS.register(bus);
     }
 }
