915473901d912313b83ccfebc6863b5333239e70
wangand1145 <3844896275@qq.com>
2026-08-25 11:23:23 +0800

    Create ModItemsMagic.java

---
+++ src/main/java/com/wangand1145/tetra_ultimate_material/ModItemsMagic.java
@@ -0,0 +1,16 @@
+package com.wangand1145.tetra_ultimate_material.registry;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItemsMagic {
+    public static final DeferredRegister<Item> ITEMS =
+        DeferredRegister.create(ForgeRegistries.ITEMS, "tetra_ultimate_material");
+
+    public static void register(IEventBus bus) {
+        ITEMS.register(bus);
+    }
+}
+
