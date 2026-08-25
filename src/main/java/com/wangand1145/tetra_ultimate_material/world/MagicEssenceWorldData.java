c963b77b303d233e361ab9e2a3e92c33e011302e
wangand1145 <3844896275@qq.com>
2026-08-25 10:37:11 +0800

    Update MagicEssenceWorldData.java

--- src/main/java/com/wangand1145/tetra_ultimate_material/world/MagicEssenceWorldData.java
+++ src/main/java/com/wangand1145/tetra_ultimate_material/world/MagicEssenceWorldData.java
@@ -1,43 +1,36 @@
 package com.wangand1145.tetra_ultimate_material.world;
 
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.saveddata.SavedData;
 
 public class MagicEssenceWorldData extends SavedData {
-    private static final String DATA_NAME = "tetra_ultimate_material_magic_essence";
-    private static final String KEY_ACTIVATED = "activated";
-
-    private boolean activated = false;
+    private static final String ID = "tetra_ultimate_material_magic_essence";
+    public boolean activated = false;
+    public static boolean clientActivated = false;
 
     public static MagicEssenceWorldData get(ServerLevel level) {
         return level.getDataStorage().computeIfAbsent(
-                MagicEssenceWorldData::load,
-                MagicEssenceWorldData::new,
-                DATA_NAME);
+            MagicEssenceWorldData::load,
+            MagicEssenceWorldData::new,
+            ID);
     }
 
-    private MagicEssenceWorldData() {}
-
-    private static MagicEssenceWorldData load(CompoundTag tag) {
+    public static MagicEssenceWorldData load(net.minecraft.nbt.CompoundTag tag) {
         MagicEssenceWorldData data = new MagicEssenceWorldData();
-        data.activated = tag.getBoolean(KEY_ACTIVATED);
+        data.activated = tag.getBoolean("activated");
+        clientActivated = data.activated;
         return data;
     }
 
     @Override
-    public CompoundTag save(CompoundTag tag) {
-        tag.putBoolean(KEY_ACTIVATED, activated);
+    public net.minecraft.nbt.CompoundTag save(net.minecraft.nbt.CompoundTag tag) {
+        tag.putBoolean("activated", activated);
         return tag;
     }
 
-    public boolean isActivated() {
-        return activated;
-    }
-
-    public void setActivated(boolean activated) {
-        this.activated = activated;
+    public void setActivated(boolean value) {
+        this.activated = value;
+        clientActivated = value;
         setDirty();
     }
 }
-
