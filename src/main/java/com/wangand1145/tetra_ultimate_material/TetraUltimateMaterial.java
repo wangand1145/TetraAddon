06f42f113f0415b57e4675fe3a1e68a91e492e4d
wangand1145 <3844896275@qq.com>
2026-08-25 23:58:54 +0800

    Update TetraUltimateMaterial.java

--- src/main/java/com/wangand1145/tetra_ultimate_material/TetraUltimateMaterial.java
+++ src/main/java/com/wangand1145/tetra_ultimate_material/TetraUltimateMaterial.java
@@ -6,15 +6,17 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod("tetra_ultimate_material")
+@Mod(TetraUltimateMaterial.MODID)  // ← 改用常量
 public class TetraUltimateMaterial {
+
+    public static final String MODID = "tetra_ultimate_material";  // ← 新增这一行
 
     public TetraUltimateMaterial() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.ITEMS.register(bus);      // 物品
         ModBlocks.BLOCKS.register(bus);    // 方块
-        ModRecipes.register(bus);          // 配方序列化器 ← 新加的这一行
-        ModItemsMagic.register(bus);      
+        ModRecipes.register(bus);          // 配方序列化器
+        ModItemsMagic.register(bus);       // 魔容源质
     }
 }
