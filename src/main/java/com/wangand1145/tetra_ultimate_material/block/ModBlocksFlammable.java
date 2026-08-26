affcf1e485805ace0e77cbd067390fb574969d15
wangand1145 <3844896275@qq.com>
2026-08-26 09:01:09 +0800

    Update ModBlocksFlammable.java

--- src/main/java/com/wangand1145/tetra_ultimate_material/block/ModBlocksFlammable.java
+++ src/main/java/com/wangand1145/tetra_ultimate_material/block/ModBlocksFlammable.java
@@ -1,14 +1,15 @@
-package com.wangand1145.tetra_ultimate_material.block;
+package com.wangand1145.tetra_ultimate_material.Block; // 若包名是 Block 则保留，否则改为 block
 
-import net.minecraftforge.eventbus.api.IEventBus;
+import com.wangand1145.tetra_ultimate_material.block.ModBlocks;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.FireBlock;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraft.world.level.block.Block;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod.EventBusSubscriber(modid = "tetra_ultimate_material", bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModBlocksFlammable {
-    @net.minecraftforge.eventbus.api.SubscribeEvent
+    @SubscribeEvent
     public static void onCommonSetup(FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
             registerFlammable(ModBlocks.FLOURISHING_LOG.get(), 5, 20);
@@ -16,9 +17,9 @@ public class ModBlocksFlammable {
         });
     }
 
-    private static void registerFlammable(Block block, int encouragement, int flammability) {
-        net.minecraftforge.common.extensions.IForgeBlock forgeBlock =
-            (net.minecraftforge.common.extensions.IForgeBlock) block;
-        forgeBlock.setFlammable(encouragement, flammability);
+    private static void registerFlammable(net.minecraft.world.level.block.Block block, int encouragement, int flammability) {
+        FireBlock fire = (FireBlock) Blocks.FIRE;
+        fire.setFlammable(block, encouragement, flammability);
     }
 }
+
